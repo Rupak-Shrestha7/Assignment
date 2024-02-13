99a3ad51572e6fa1d4035483b0446d44da2f38b7
JS <rupakshrestha1111@gmail.com>
2024-02-14 04:10:21 +0545

    First Commit

---
+++ Assignment2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a whole number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+                {
+                    Console.Write("The number is positive ");
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine("and even");
+                    }
+                    else
+                    {
+                        Console.WriteLine("and odd");
+                    }
+                }
+                else if (number < 0)
+                {
+                    Console.Write("The number is negative ");
+                    if (number % 2 == 0)
+                    {
+                        Console.WriteLine("and even");
+                    }
+                    else
+                    {
+                        Console.WriteLine("and odd");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The number is zero (and it is even)");
+                }
+            }
+        }
+}
